26b4e53158768e7fd1149cce69e64136ccc73cd3
Fynce <43083168+angellicburst@users.noreply.github.com>
2019-01-03 17:40:35 +0800

    外置Tomcat

--- src/main/java/com/hbnu/gradesign/GraDesignApplication.java
+++ src/main/java/com/hbnu/gradesign/GraDesignApplication.java
@@ -2,9 +2,17 @@ package com.hbnu.gradesign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class GraDesignApplication {
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class GraDesignApplication extends SpringBootServletInitializer {
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GraDesignApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(GraDesignApplication.class, args);
