a2aa10642d13583e9110e2f95be2ed12fa50f484
Fynce <43083168+angellicburst@users.noreply.github.com>
2019-01-03 17:02:38 +0800

    commit

---
+++ src/main/java/com/hbnu/gradesign/GraDesignApplication.java
@@ -0,0 +1,14 @@
+package com.hbnu.gradesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraDesignApplication.class, args);
+	}
+
+}
+
